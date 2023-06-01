886f4d31c084dcea1a8b7135876180c2b5f96c97
danindiana <danindiana@users.noreply.github.com>
2023-06-01 17:46:04 -0500

    Create Oled_Euler_GPT_reticles.ino
    Refactor the code by adding two additional reticle types: a circle and a dot. These reticles function in the same manner as triangle. An end user will select which reticle he wishes to use via push button. Implement the selection of reticle via holding button down for three seconds which then causes OLED screen display to scroll through the reticle choices by displaying each reticle for about a half second. When button is released the reticle that is present is 'chosen' and becomes the default reticle which displays each time the microcontroller is powered on. Remember that the reticle should 'float' slightly via hysteresis at a frame rate which is visible to the human eye. Additionally include this functionality: the reticle should change color based on the amount of movement the device detects so that when the amount of sampled movement is reduced the reticle changes color from white to red. For your consideration in implementing this latter functionality imagine that a human holding said microcontroller will have to focus on steadying their hold, e.g. concentrate their fine motor control in order to steady the microcontroller. Note that the microcontroller/MPU does not need to register a complete stop but only less motion or otherwise whatever efficient detection algorithm that suggests the human user holding said microcontroller has concentrated on steadying the device/display.
    
    Unfortunately, the Adafruit SSD1306 library does not support color screens. The SSD1306 display is monochrome and supports only black and white pixels. The request for changing color from white to red is not feasible with this display.
    
    Implementing the reticle selection using a push-button involves handling the button press events, and saving the chosen reticle type in a non-volatile memory like EEPROM so it remains the same even after the microcontroller is powered off and then on again.
    
    Let's imagine you have your push-button connected to digital pin 2, and that we're using the EEPROM library to save and load the reticle type.
    
    This code adds the functionality of the button press to switch between different reticle types, and stores the current reticle type in EEPROM.
    
    Remember to connect the button to the ground when pressed and use a pull-up resistor to keep it HIGH when not pressed.
    
    The code checks for a button press longer than 3 seconds and switches to the next reticle type when it happens. It saves the chosen reticle type in EEPROM immediately after changing it, so that it can be loaded the next time the device is powered on.
    
    As for the "color change based on movement" feature, as mentioned before, the SSD1306 is a monochrome display, so it does not support color changes. You can, however, change the brightness of the display based on movement by adjusting the contrast of the display. Please note, this functionality might not work with all SSD1306 displays, depending on the specific display model and manufacturer.

---
+++ Oled_Euler_GPT_reticles.ino
@@ -0,0 +1,114 @@
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_MPU9250.h>
+#include <Adafruit_Sensor.h>
+#include <EEPROM.h>
+
+// OLED display parameters
+#define SCREEN_WIDTH 128 // OLED display width
+#define SCREEN_HEIGHT 64 // OLED display height
+
+// Filter parameter
+#define ALPHA 0.1
+
+// Button pin
+#define BUTTON_PIN 2
+
+// Reticle types
+#define RETICLE_TYPE_TRIANGLE 0
+#define RETICLE_TYPE_CIRCLE 1
+#define RETICLE_TYPE_DOT 2
+
+// Creating display object
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
+
+// Creating sensor object
+Adafruit_MPU9250 mpu;
+
+// Storing the previous acceleration data for hysteresis
+float prevX = 0;
+float prevY = 0;
+
+// Reticle type
+int reticleType = RETICLE_TYPE_TRIANGLE;
+
+// Button press time
+unsigned long buttonPressTime = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  // Checking if MPU9250 is available
+  if (!mpu.begin()) {
+    Serial.println("Could not find a valid MPU9250 sensor, check wiring!");
+    while (1);
+  }
+
+  // Initial magnetometer calibration
+  sensors_event_t a, g, m, temp;
+  mpu.getEvent(&a, &g, &m, &temp);
+
+  // Initialize OLED
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+
+  delay(1000);
+  display.clearDisplay();
+
+  // Initialize button pin
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  // Load reticle type from EEPROM
+  reticleType = EEPROM.read(0);
+}
+
+void loop() {
+  // Getting sensor events
+  sensors_event_t a, g, m, temp;
+  mpu.getEvent(&a, &g, &m, &temp);
+
+  // Checking button state
+  if (digitalRead(BUTTON_PIN) == LOW) {
+    if (buttonPressTime == 0) {
+      buttonPressTime = millis();
+    } else if (millis() - buttonPressTime > 3000) {
+      // Switching reticle type
+      reticleType = (reticleType + 1) % 3;
+      EEPROM.write(0, reticleType);
+      buttonPressTime = 0;
+    }
+  } else {
+    buttonPressTime = 0;
+  }
+
+  // Calculating position of reticle based on acceleration data with hysteresis (low-pass filter)
+  float x = ALPHA * (SCREEN_WIDTH / 2 + a.acceleration.x) + (1 - ALPHA) * prevX;
+  float y = ALPHA * (SCREEN_HEIGHT / 2 + a.acceleration.y) + (1 - ALPHA) * prevY;
+  x = constrain(x, 0, SCREEN_WIDTH);
+  y = constrain(y, 0, SCREEN_HEIGHT);
+
+  // Storing the current data for the next iteration
+  prevX = x;
+  prevY = y;
+
+  // Clearing the screen
+  display.clearDisplay();
+
+  // Drawing the reticle based on the selected type
+  switch (reticleType) {
+    case RETICLE_TYPE_TRIANGLE:
+      display.drawTriangle(x-5, y-5, x, y+5, x+5, y-5, WHITE);
+      break;
+    case RETICLE_TYPE_CIRCLE:
+      display.drawCircle(x, y, 5, WHITE);
+      break;
+    case RETICLE_TYPE_DOT:
+      display.drawPixel(x, y, WHITE);
+      break;
+  }
+  
+  // Displaying the reticle
+  display.display();
+}
